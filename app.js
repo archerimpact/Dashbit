9b355299d241eec6d38038c8293bbe1c30332fe4
rhayes128 <ryan.hayes128@gmail.com>
2017-05-30 15:43:13 +0000

    stable ready for release without graphs or report

--- app.js
+++ app.js
@@ -1,182 +1,43 @@
-var express = require('express');
-var app = express();
-var firebase = require('firebase');
-var _ = require('underscore-node');
-app.set("view engine", "ejs");
-app.use(express.static('public'));
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-var blockexplorer = require('blockchain.info/blockexplorer');
-var exchange = require('blockchain.info/exchange');
-
-var config = {
-    apiKey: "AIzaSyCVDXv97plY6ExZW-vnwOYBIrg0vu5PhYY",
-    authDomain: "blockchainsquad.firebaseapp.com",
-    databaseURL: "https://blockchainsquad.firebaseio.com",
-    projectId: "blockchainsquad",
-    storageBucket: "blockchainsquad.appspot.com",
-    messagingSenderId: "969026353343"
-  };
-firebase.initializeApp(config);
-
-var labels = {};
-
-
-app.get("/", function(req, res) {
-    var user = firebase.auth().currentUser;
-    var addresses = [];
-    var balances = {};
-    if (user) {
-        var username = user.email.split("@")[0];
-        firebase.database().ref('/Users/' + username).once('value').then(function(snapshot) {
-            if (snapshot.val() != null) {
-                for (var key in snapshot.val()) {
-                    var addr = snapshot.val()[key]["Address"];
-                    var label = snapshot.val()[key]["Label"];
-                    var n = snapshot.val()[key]["NumTx"];
-                    labels[addr] = label;
-                    var obj = {
-                        addr: addr,
-                        label: label,
-                        num: n
-                    }
-                    addresses.push(obj);
-                }
-                var finished = _.after(addresses.length, doRender);
-                for (var i = 0; i < addresses.length; i++) {
-                    blockexplorer.getAddress(addresses[i].addr).then(function(obj) {
-                        var b = (obj.final_balance / 100000000);
-                        var n = obj.n_tx;
-                        exchange.fromBTC(obj.final_balance, "USD").then(function(dollars) {
-                            balances[obj.address] = {
-                                btc: b,
-                                usd: dollars,
-                                ntx: n
-                            };
-                            finished();
-                        })
-                    });
-                }
-            } else {
-                doRender();
-            }
-        });
-	    
-    } else {
-        res.redirect('/login');
-    }
-    function doRender() {
-	   res.render('index', {email: user.email, data: addresses, balances: balances});
-	}
-});
-
-app.get('/login', function(req, res) {
-    res.render('login');
-});
-
-app.post('/logOut', function(req, res) {
-    firebase.auth().signOut().then(function() {
-  		// Sign-out successful.
-	}).catch(function(error) {
-		alert(error.message);
-	});
-    res.redirect('/');
-})
-
-app.post('/login', function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    var failed = false;
-    firebase.auth()
-        .signInWithEmailAndPassword(email, password)
-        .then(function(user) { res.redirect('/'); })
-        .catch(function(){});
-});
-
-app.get('/address/:addr', function(req, res) {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        var username = user.email.split("@")[0];
-        var addr = req.params.addr;
-        blockexplorer.getAddress(addr).then(function(data) {
-            firebase.database().ref('/Users/' + username).once('value').then(function(snapshot) {
-                var k = "";
-                for (var key in snapshot.val()) {
-                    var btc = snapshot.val()[key]["Address"];
-                    if (addr == btc) {
-                        k = key;
-                    }
-                }
-                var dbData = "";
-                if (k === "") {
-                    dbData = "";
-                } else {
-                    firebase.database().ref().child("Users").child(username).child(k).child("NumTx").set(data.n_tx);
-                    dbData = snapshot.val()[k];
-                }
-                res.render('individual', {btcData: data, dbData: dbData, labels: labels, blockexplorer: blockexplorer});
-            });
-        });
-    } else {
-        res.redirect('/login');
-    }
-});
-
-app.get('/add', function(req, res) {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        res.render("addAddress");
-    } else {
-      res.redirect('/login');  
-    }
+var _                       = require('underscore-node'),
+    express                 = require('express'),
+    app                     = express(),
+    bodyParser              = require('body-parser'),
+    blockexplorer           = require('blockchain.info/blockexplorer'),
+    exchange                = require('blockchain.info/exchange'),
+    mongoose                = require('mongoose'),
+    passport                = require('passport'),
+    LocalStrategy           = require('passport-local'),
+    passportLocalMongoose   = require('passport-local-mongoose'),
+    User                    = require('./models/user'),
+    methodOverride          = require('method-override');;
     
-});
+var utilRoutes = require('./routes/util'),
+    addressRoutes = require('./routes/address');
 
-app.post('/add', function(req, res) {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        var firebaseRef = firebase.database().ref();
-        var username = user.email.split("@")[0];
-        var label = req.body.label;
-        var address = req.body.address;
-        blockexplorer.getAddress(address).then(function(obj) {
-            firebaseRef.child("Users").child(username).push({
-        		"Label" : label,
-        		"Address" : address,
-        		"Description" : "",
-        		"Notes" : "",
-        		"NumTx" : obj.n_tx
-        	});
-        	res.redirect('/');
-        });
-    }
+mongoose.connect("mongodb://localhost/blockchain_dashboard");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+//  Passport Config
+app.use(require('express-session')({
+    secret: 'your eyes, they turn me',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
 });
-
-app.post('/createAccount', function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    var confirmpassword = req.body.confirm;
-	if (email != '' && password != '') {
-		firebase.auth()
-		.createUserWithEmailAndPassword(email, password)
-		.then(function(user) {
-		    firebase.auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(function(user) { 
-                var username = email.split("@")[0];
-                res.redirect('/'); 
-            })
-            .catch(function(){});
-		        res.redirect('/login');
-		    })
-		.catch(function(error) {
-		    console.log(error);
-		    res.redirect('/login');
-		});
-	}
-});
-
-app.get('/address/:addr/report/:txindex', function(req, res) {
+app.use(addressRoutes);
+app.use(utilRoutes);
+app.get('/addresses/:addr/report/:txindex', function(req, res) {
+    res.render('txreport');
     var importantInputs = [];
     var rootAddr = req.params.addr;
     var minInputDepth = 10000000000;
@@ -241,11 +102,6 @@ app.get('/address/:addr/report/:txindex', function(req, res) {
         }
     });
 });
-
-app.post('/search', function(req, res) {
-    var addr = req.body.searchAddr;
-    res.redirect('/address/' + addr);
-})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server has started!");
